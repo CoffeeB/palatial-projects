7fa63ed0a26eeffb95834ee496811beba4eb2909
Jesse Abafoni <jesseinreasns3@gmail.com>
2024-11-01 12:19:10 +0000

    added bootstrap and boxicons

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,9 +2,25 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
+    <Html lang="en" data-bs-theme="dark">
+      <Head>
+        <link rel="icon" type="" href="/logo.png" />
+
+        <link rel="stylesheet" href="/assets/css/styles.css" />
+        <link rel="stylesheet" href="/assets/css/bootstrap-btn.min.css" />
+        <link
+          rel="stylesheet"
+          href="/assets/css/boxicons/css/boxicons.min.css"
+        />
+        <link rel="stylesheet" href="/assets/css/boxicons/css/boxicons.css" />
+        <link rel="stylesheet" href="/assets/css/boxicons/css/animations.css" />
+        <link
+          rel="stylesheet"
+          href="/assets/css/boxicons/css/transformations.css"
+        />
+        <script src="/assets/js/theme.min.js" async></script>
+      </Head>
+      <body data-bs-theme="dark">
         <Main />
         <NextScript />
       </body>
